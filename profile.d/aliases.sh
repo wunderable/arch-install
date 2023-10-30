6847b60ff165e9c53ae0e2bba01e4aa767a2863a
wunderable <33606930+wunderable@users.noreply.github.com>
2023-10-30 15:27:45 -0500

    Create aliases.sh

---
+++ profile.d/aliases.sh
@@ -0,0 +1,19 @@
+alias ls='ls --color=auto'
+alias ll='ls -hAl'
+alias la='ls -a'
+
+alias ls..='ls ..'
+alias ll..='ll ..'
+alias la..='la ..'
+alias cd..='cd ..'
+
+alias view='vim -R'
+alias bat='cat /sys/class/power_supply/BAT0/capacity'
+alias term='ps -o "cmd=" -p $(ps -o "ppid=" -p $$)'
+
+alias date_y='date +%F' # YYYY-MM-DD
+alias date_t='date +%T' # HH:MM:SS
+alias date_yt='date +%F\ %T' # YYYY-MM-DD HH:MM:SS
+alias date_Y='date +%4Y%m%d' # YYYYMMDD
+alias date_T='date +%H%M%S' # HHMMSS
+alias date_YT='date +%4Y%m%d_%H%M%S' # YYYYMMDD_HHMMSS
