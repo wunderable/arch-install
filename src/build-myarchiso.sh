a207065b659d11396be0a013321fde8b2ab95e73
wunderable <33606930+wunderable@users.noreply.github.com>
2024-01-30 16:15:53 -0600

    Update build-myarchiso.sh

--- src/build-myarchiso.sh
+++ src/build-myarchiso.sh
@@ -7,12 +7,19 @@ if [[ $EUID -ne 0 ]]; then
   exit
 fi
 
-# We used to just download the latest ISO from a mirror
-#mkdir -p /boot/iso
-#curl https://mirrors.edge.kernel.org/archlinux/iso/latest/archlinux-x86_64.iso -o /boot/iso/myarch.iso
+pacman -Syy
+pacman --noconfirm -S archiso
 
 cp -r /usr/share/archiso/configs/releng /tmp/iso
+mkdir -p /tmp/iso/airootfs/usr/local/bin
+mkdir -p /boot/iso
+echo -e 'btrfs-progs\nintel-ucode\niwc\nvim' >> /tmp/iso/packages.x86_64
 cp /usr/local/src/iso-cmds.sh /tmp/iso/airootfs/usr/local/bin/cmds
 sed -i '/^file_permissions=(/a \ \ ["/usr/local/bin/cmds"]="0:0:755"' /tmp/iso/profiledef.sh
 mkdir -p /boot/iso
-mkarchiso -v -w /tmp/iso -o /boot/iso/myarch.iso /tmp/iso
+mkarchiso -v -w /tmp/iso -o /tmp/out /tmp/iso
+
+rm -rf /tmp/iso
+[ ! -e /boot/iso/myarch.iso ] || rm /boot/iso/myarch.iso
+mv /tmp/out/archlinux-*.iso /boot/iso/myarch.iso
+rmdir /tmp/out
