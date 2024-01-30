60e42216290df7024f25f5599e358693de1ed98a
wunderable <33606930+wunderable@users.noreply.github.com>
2024-01-30 12:30:28 -0600

    Using custom ISO instead of stock

--- src/grub-update-iso.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $EUID -ne 0 ]]; then
-  echo "Permission denied"
-  exit
-fi
-
-mkdir -p /boot/iso
-curl https://mirrors.edge.kernel.org/archlinux/iso/latest/archlinux-x86_64.iso -o /boot/iso/archlinux-x86_64.iso
