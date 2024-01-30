e004d7e60bbc6683a539f672bba87b6c83e360b3
wunderable <33606930+wunderable@users.noreply.github.com>
2024-01-30 17:09:05 -0600

    Added mfs script and fixed hook to allow loop mount

--- src/build-myarchiso.sh
+++ src/build-myarchiso.sh
@@ -10,12 +10,15 @@ fi
 pacman -Syy
 pacman --noconfirm -S archiso
 
-cp -r /usr/share/archiso/configs/releng /tmp/iso
+cp -r /usr/share/archiso/configs/baseline /tmp/iso
 mkdir -p /tmp/iso/airootfs/usr/local/bin
 mkdir -p /boot/iso
 echo -e 'btrfs-progs\nintel-ucode\niwc\nvim' >> /tmp/iso/packages.x86_64
 cp /usr/local/src/iso-cmds.sh /tmp/iso/airootfs/usr/local/bin/cmds
+cp /usr/local/src/mfs.sh /tmp/iso/airootfs/usr/local/bin/mfs
 sed -i '/^file_permissions=(/a \ \ ["/usr/local/bin/cmds"]="0:0:755"' /tmp/iso/profiledef.sh
+sed -i '/^file_permissions=(/a \ \ ["/usr/local/bin/mfs"]="0:0:755"' /tmp/iso/profiledef.sh
+sed -i 's/archiso/archiso archiso_loop_mnt/' /tmp/iso/airootfs/etc/mkinitcpio.conf.d/archiso.conf
 mkdir -p /boot/iso
 mkarchiso -v -w /tmp/iso -o /tmp/out /tmp/iso
 
