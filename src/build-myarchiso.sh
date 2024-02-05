526d2894e5aab15b283a80dd7f18e097490ac4fc
wunderable <33606930+wunderable@users.noreply.github.com>
2024-02-05 14:15:38 -0600

    Autologin and permissions added via loop

--- src/build-myarchiso.sh
+++ src/build-myarchiso.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script will create a bootable ISO containing a few custom scripts
+# Script will create a bootable ISO containing a few custom scripts to make managing btrfs easier
 
 # Ensure root permissions
 if [[ $EUID -ne 0 ]]; then
@@ -22,14 +22,21 @@ echo -e 'btrfs-progs\niwc\nvim' >> /tmp/iso/packages.x86_64
 mkdir -p /tmp/iso/airootfs/etc/systemd/system/getty@tty1.service.d
 cp /usr/share/archiso/configs/releng/airootfs/etc/systemd/system/getty@tty1.service.d/autologin.conf /tmp/iso/airootfs/etc/systemd/system/getty@tty1.service.d
 
-# Include our own scripts
+# Include our aliases
+mkdir -p /tmp/iso/airootfs/etc/profile.d
+cp /etc/profile.d/aliases.sh /tmp/iso/airootfs/etc/profile.d
+
+# Include our scripts
 mkdir -p /tmp/iso/airootfs/usr/local/bin
 cp /usr/local/src/iso-cmds.sh /tmp/iso/airootfs/usr/local/bin/cmds
 cp /usr/local/src/iso-mfs.sh /tmp/iso/airootfs/usr/local/bin/mfs
+cp /usr/local/src/subv.py /tmp/iso/airootfs/usr/local/bin/subv
 
-# Set permissions for our scripts (I should turn this into a loop)
-sed -i '/^file_permissions=(/a \ \ ["/usr/local/bin/cmds"]="0:0:755"' /tmp/iso/profiledef.sh
-sed -i '/^file_permissions=(/a \ \ ["/usr/local/bin/mfs"]="0:0:755"' /tmp/iso/profiledef.sh
+# Set permissions for our scripts
+for CMD in /tmp/iso/airootfs/usr/local/bin/*; do
+  CMD=$(basename -- "$CMD")
+  sed -i '/^file_permissions=(/a \ \ ["/usr/local/bin/'$CMD'"]="0:0:755"' /tmp/iso/profiledef.sh
+done
 
 # Allow ISO to be mounted via loop
 sed -i 's/archiso/archiso archiso_loop_mnt/' /tmp/iso/airootfs/etc/mkinitcpio.conf.d/archiso.conf
