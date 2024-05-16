18df6f651ff141700bf5a771f871a2d0b2efa378
wunderable <danaltmann@gmail.com>
2024-05-16 11:17:47 -0500

    Updated subv

--- src/subv.py
+++ src/subv.py
@@ -1,139 +1,176 @@
 #! /usr/bin/python
 
-import sys
 import re
+import sys
+import os.path
 import argparse
 import subprocess
-from datetime import datetime
+import numpy as np
 
-class CustomArgumentParser(argparse.ArgumentParser):
-    def __init__(self, *args, **kwargs):
-        super(self.__class__, self).__init__(*args, **kwargs)
-        self.subs = {}
-    def error(self, message):
-        sys.stderr.write('error: %s\n' % message)
-        self.print_help()
-        sys.exit(2)
+# Formatter for argparse that preservers new lines if text begins with 'R|'
+class MyFormatter(argparse.HelpFormatter):
+    def _split_lines(self, text, width):
+        if text.startswith('R|'):
+            return text[2:].splitlines()
+        return argparse.HelpFormatter._split_lines(self, text, width)
 
-def get_subvolumes():
-    subvolumes_string = subprocess.run(
-            ['sudo', 'btrfs', 'subvolume', 'list', '-p', '/'],
-            stdout=subprocess.PIPE).stdout.decode('utf-8')
+# Get argparse instance for handling supplied arguments
+def get_parser():
+    main = argparse.ArgumentParser(
+            prog='subv',
+            description='A utility to help manage btrfs subvolumes',
+            formatter_class=MyFormatter)
+    main.subs = {}
+    subs = main.add_subparsers(help='commands', dest='cmd')
+
+    # List subparser
+    listp = subs.add_parser('list',
+                            help='list all subvolumes under /',
+                            description='Outputs a list of all subvolumes under the / directory',
+                            formatter_class=MyFormatter)
+    main.subs['list'] = listp
+    listp.add_argument('-f', '--fields',
+                       default='ipnm',
+                       help='R|FIELDS is a series of letters that refer to the fields that will be displayed and in what order. Default is \'ipnm\'. Fields can be:\n  i  id\n  p  parent\n  n  name\n  m  mount')
+    listp.add_argument('-s', '--sort',
+                       choices=['i','p','n','m'],
+                       default='i',
+                       help='SORT is a letter indicating which field to sort by. Default is \'i\'')
+
+    # Snap subparser
+    snapp = subs.add_parser('snap',
+                            help='create a snapshot of the specified subvolumes',
+                            description='Creates a snapshot of all specified subvolumes and saves it to /.snapshots',
+                            formatter_class=MyFormatter)
+    main.subs['snap'] = snapp
+    snapp.add_argument('-n', '--name', help='append NAME to the snapshot\'s filename')
+    snapp.add_argument('sources', nargs='*',#argparse.REMAINDER,
+                       help='R|the source locations to create a snapshot of. Can either be a single argument made up of only valid letter abbreviations or a list of names separated by a space\n  r  root    /\n  h  home    /home\n  l  logs    /var/logs')
+
+    return main
+
+# Additional argument verification that argparse can't natively handle
+def verify_args(parser, args):
+    match(args.cmd):
+
+        case 'list':
+            if args.fields != None and not re.match("^[imnp]+$", args.fields):
+                parser.subs['list'].error('Unknow value [' + args.fields + '] for FIELDS')
+
+        case 'snap':
+            if len(args.sources) == 1 and re.match("^[hlr]+$", args.sources[0]):
+                print('abbrevs are good')
+                return
+            for s in args.sources:
+                if not s in ['home', 'logs', 'root']:
+                    parser.subs['snap'].error('Unknown source [' + s + ']')
+
+def read_config():
+    try:
+        file = open(os.path.expandvars("$HOME/.config/subv/sources.cfg"), "r")
+        for line in file:
+            print(line)
+
+    except:
+        print('ERROR: There was an issue reading $HOME/.config/subv/sources.cfg', file=sys.stderr)
+        exit(1)
+
+    return True
+
+def execute(cmd):
+    return subprocess.run(cmd.split(), stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+def get_subs():
+    results = execute('sudo btrfs subvolume list -a /')
     subvolumes = []
-    for line in subvolumes_string.splitlines():
+    for line in results.splitlines():
         field = line.split()
-        subvolumes.append((int(field[1]), int(field[5]), field[10]))
+        field[8] = re.sub(r'^<FS_TREE>/', '', field[8])
+        subvolumes.append((field[1], field[6], field[8]))
     return subvolumes
 
-def get_mounts():
-    mounts_string = subprocess.run(
-            ['findmnt', '-nl', '-tbtrfs', '-osource,target'],
-            stdout=subprocess.PIPE).stdout.decode('utf-8')
-    mounts = []
-    for line in mounts_string.splitlines():
+def get_snaps():
+    results = execute('sudo btrfs subvolume list -s /')
+    snaps = []
+    for line in results.splitlines():
         field = line.split()
-        match = re.search(r'\[/(.*?)\]', field[0])
+        snaps.append(field[1])
+    return snaps
+
+def get_ros():
+    results = execute('sudo btrfs subvolume list -r /')
+    ros = []
+    for line in results.splitlines():
+        field = line.split()
+        ros.append(field[1])
+    return ros
+
+def get_mounts():
+    results = execute('findmnt -nl -tbtrfs -ooptions,target')
+    mounts = []
+    for line in results.splitlines():
+        field = line.split()
+        match = re.search(r'subvolid=(\d+)', field[0])
         mounts.append((match.group(1), field[1]))
     return mounts
 
-def snap(args):
-    if not (args.home or args.etc or args.root):
-        sys.stderr.write('error: required at least one of (-h | -e | -r)\n')
-        parser.subs['snap'].print_help()
-        sys.exit(2)
+def get_subvolumes():
+    subs = get_subs()
+    snaps = get_snaps()
+    ros = get_ros()
+    mounts = get_mounts()
+    subvolumes = []
+    for s in subs:
+        typ = 'Subvolume'
+        if s[0] in snaps:
+            typ = 'Snapshot'
+            snaps.remove(s[0])
+        r = 'rw'
+        if s[0] in ros:
+            r = 'ro'
+            ros.remove(s[0])
+        mnt = '-'
+        for m in mounts:
+            if m[0] == s[0]:
+                mnt = m[1]
+                mounts.remove(m)
+                break
+        subvolumes.append((int(s[0]), int(s[1]), s[2], typ, r, mnt))
+    print_2d(subvolumes, 3)
 
-    name = datetime.now().strftime('%Y-%m-%d_%H%M%S')
-    if args.name:
-        name += '_' + args.name
+def print_2d(arr, spaces=1):
+    lengths = []
+    first = True
+    for i in arr:
+        c = -1 
+        for j in i:
+            c += 1
+            if first: lengths.append(0)
+            if len(str(j)) > lengths[c]: lengths[c] = len(str(j))
+        first = False
+    for i in arr:
+        c = -1
+        for j in i:
+            c += 1
+            print(j, end='')
+            print(' ' * (lengths[c] - len(str(j)) + spaces), end='')
+        print()
 
-    source = ''
-    destination = ''
-    if args.home:
-        source = '/home'
-        destination = '/snapshots/home/' + name
-    if args.etc:
-        source = '/etc'
-        destination = '/snapshots/etc/' + name
-    if args.root:
-        source = '/'
-        destination = '/snapshots/root/' + name
-
-    subprocess.run(['sudo', 'btrfs', 'subvolume', 'snapshot', source, destination])
-
-def list(args):
-    if args.tree:
-        print_tree()
-        sys.exit(0)
-    subvolumes = get_subvolumes()
-    line_format = '{:<6} {:<6} {}'
-    print(line_format.format('ID', 'Parent', 'Name'))
-    if args.sort_id:
-        subvolumes.sort()
-    if args.sort_parent:
-        subvolumes.sort(key=lambda x: (x[1], x[2]))
-    if args.sort_name:
-        subvolumes.sort(key=lambda x: x[2])
-#    subvolumes.insert(0, (5, 0, 'TOP'))
-    for item in subvolumes:
-        print(line_format.format(item[0], item[1], item[2]))
-
-def delete(args):
-    if args.id:
-        subprocess.run(['sudo', 'btrfs', 'subvolume', 'delete', '--subvolid', str(args.id), '/'])
-        sys.exit(0)
-    print('delete')
-
-def print_tree():
-#    subvolumes = get_subvolumes()
-#    mounts = get_mounts()
-    print('tree')
-
-def get_parser():
-    main_parser = CustomArgumentParser(prog='subv', description='Utitlity to help manage btrfs subvolumes', add_help=False)
-    main_parser.add_argument('--help', action='help', help='show this help message and exit')
-    sub_parsers = main_parser.add_subparsers(title='subcommands')
-
-    snap_parser = sub_parsers.add_parser('snap', help='create a snapshot', description='Creates a snapshot', add_help=False)
-    snap_parser.add_argument('-h', '--home', dest='home', action='store_true', help='create a snapshot of /home')
-    snap_parser.add_argument('-e', '--etc', dest='etc', action='store_true', help='create a snapshot of /etc')
-    snap_parser.add_argument('-r', '--root', dest='root', action='store_true', help='create a snapshot of /root')
-    snap_parser.add_argument('-n', '--name', dest='name', help='appends NAME to the snapshot\'s name')
-    snap_parser.add_argument('--help', action='help', help='show this help message and exit')
-    snap_parser.set_defaults(func=snap)
-    main_parser.subs['snap'] = snap_parser
-
-    list_parser = sub_parsers.add_parser('list', help='display subvolumes', description='Display subvolumes', add_help=False)
-    list_group = list_parser.add_mutually_exclusive_group()
-    list_group.add_argument('-t', '--tree', dest='tree', action='store_true', help='format output as a tree')
-    list_group.add_argument('-i', '--id', dest='sort_id', action='store_true', help='sort output by id number')
-    list_group.add_argument('-p', '--parent', dest='sort_parent', action='store_true', help='sort output by parent\'s id number')
-    list_group.add_argument('-n', '--name', dest='sort_name', action='store_true', help='sort output by name')
-    list_parser.add_argument('--help', action='help', help='show this help message and exit')
-    list_parser.set_defaults(func=list)
-    main_parser.subs['list'] = list_parser
-
-    delete_parser = sub_parsers.add_parser('delete', help='delete subvolumes', description='Delete subvolumes', add_help=False)
-    delete_group = delete_parser.add_mutually_exclusive_group(required=True)
-    delete_group.add_argument('-i', '--id', dest='id', type=int, help='deletes the subvolume having ID')
-    delete_group.add_argument('-p', '--path', dest='path', help='deletes the subvolume located at PATH')
-    delete_group.add_argument('-a', '--age', dest='age', help='deletes subvolumes older than AGE')
-    delete_parser.add_argument('--help', action='help', help='show this help message and exit')
-    delete_parser.set_defaults(func=delete)
-    main_parser.subs['delete'] = delete_parser
-
-    return main_parser
-
+# Program start
 def main():
-    global parser
+    get_subvolumes()
+    exit(0)
+
     parser = get_parser()
 
-    # If no arguments are supplied, show help and exit
-    if len(sys.argv) == 1:
+    if len(sys.argv) == 1: # if no arguments are supplied
         parser.print_help()
-        sys.exit(2)
+        exit(1)
 
     args = parser.parse_args()
-    args.func(args)
+    verify_args(parser, args)
+
+    print(args)
 
 if __name__ == '__main__':
     main()
