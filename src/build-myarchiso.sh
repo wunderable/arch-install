90ed2f58d7bbcabdd1bd55b132359a0201d8b90a
wunderable <33606930+wunderable@users.noreply.github.com>
2024-06-11 14:32:19 -0500

    renamted myarch.iso to arch_boot.iso

--- src/build-myarchiso.sh
+++ src/build-myarchiso.sh
@@ -49,7 +49,7 @@ mkarchiso -v -w /tmp/iso -o /tmp/out /tmp/iso
 
 # Cleanup of files
 rm -rf /tmp/iso
-[ ! -e /boot/iso/myarch.iso ] || rm /boot/iso/myarch.iso
+[ ! -e /boot/iso/arch_boot.iso ] || rm /boot/iso/arch_boot.iso
 mkdir -p /boot/iso
-mv /tmp/out/archlinux-*.iso /boot/iso/myarch.iso
+mv /tmp/out/archlinux-*.iso /boot/iso/arch_boot.iso
 rmdir /tmp/out
