229522e01b2cac59551b1f888557c9561b0ef697
wunderable <33606930+wunderable@users.noreply.github.com>
2024-06-15 16:50:13 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -381,7 +381,7 @@ arch-chroot /mnt sh install.sh
 ##############
 
 # Hyprland setup
-cp $DIR/hyprland/hyprland.conf /mnt/home/$USER/.config/hypr
+cp $DIR/files/hypr/hyprland.conf /mnt/home/$USER/.config/hypr
 
 # Copy other miscellaneous files
 cp $DIR/files/aliases.sh /mnt/etc/profile.d/aliases.sh
