85153af923e718d74d7ad67020d0234407af0c5f
wunderable <33606930+wunderable@users.noreply.github.com>
2024-06-15 14:58:44 -0500

    forgot greetd

--- install.sh
+++ install.sh
@@ -281,7 +281,7 @@ sed -Ei "s/^# (%wheel ALL=\(ALL:ALL\) ALL)/\1/" /etc/sudoers
 sed -i "s/#Color/Color/" /etc/pacman.conf
 
 # Install packages with pacman
-pacman --noconfirm -S python3 kitty zsh otf-commit-mono-nerd ttf-noto-nerd hyprland neofetch upower gtk4 vivaldi vivaldi-ffmpeg-codecs spotify-launcher tuigreet
+pacman --noconfirm -S python3 kitty zsh otf-commit-mono-nerd ttf-noto-nerd hyprland neofetch upower gtk4 vivaldi vivaldi-ffmpeg-codecs spotify-launcher tuigreet greetd
 
 ############
 # NEOFETCH #
