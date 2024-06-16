9fb45cbdf9f1a69d386cf64441e6e54075098b7c
wunderable <33606930+wunderable@users.noreply.github.com>
2024-06-16 14:34:30 -0500

    add font-list

--- files/aliases.sh
+++ files/aliases.sh
@@ -13,6 +13,7 @@ alias cd..='cd ..'
 alias view='vim -R'
 alias bat='printf "%s%% - %s\n" $(cat /sys/class/power_supply/BAT0/capacity) $(cat /sys/class/power_supply/BAT0/status)'
 alias term='neofetch --config ~/.config/neofetch/term.conf'
+alias font-list='fc-list | cut -d ":" -f 2- | cut -d "," -f 1 | sort -u'
 
 alias date_y='date +%F' # YYYY-MM-DD
 alias date_t='date +%T' # HH:MM:SS
