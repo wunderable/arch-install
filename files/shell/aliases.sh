5798b3eb86b2495d6b9e7ca75336a5e63627fb9a
wunderable <33606930+wunderable@users.noreply.github.com>
2024-06-24 15:36:21 -0500

    Update aliases.sh

--- files/shell/aliases.sh
+++ files/shell/aliases.sh
@@ -10,13 +10,7 @@ alias ll..='ll ..'
 alias lt..='lt ..'
 alias cd..='cd ..'
 
+alias cls='clear'
 alias view='vim -R'
 alias bat='printf "%s%% - %s\n" $(cat /sys/class/power_supply/BAT0/capacity) $(cat /sys/class/power_supply/BAT0/status)'
 alias font-list='fc-list | cut -d ":" -f 2- | cut -d "," -f 1 | sort -u'
-
-alias date_y='date +%F' # YYYY-MM-DD
-alias date_t='date +%T' # HH:MM:SS
-alias date_yt='date +%F\ %T' # YYYY-MM-DD HH:MM:SS
-alias date_Y='date +%4Y%m%d' # YYYYMMDD
-alias date_T='date +%H%M%S' # HHMMSS
-alias date_YT='date +%4Y%m%d_%H%M%S' # YYYYMMDD_HHMMSS
