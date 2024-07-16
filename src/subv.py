6950c5a2ee6b9a0e3dfd2e785ebd1b2492c2cdb7
wunderable <danaltmann@gmail.com>
2024-07-16 15:15:00 -0500

    yet committed updates

--- src/subv.py
+++ src/subv.py
@@ -9,13 +9,14 @@ import configparser
 from datetime import datetime
 
 fields = [
-          ('i', 'id'),
-          ('p', 'parent\'s id'),
-          ('n', 'name'),
-          ('l', 'location'),
-          ('m', 'explictly mounted (yes or no)'),
-          ('r', 'permissions (read-write or read-only)'),
-          ('s', 'type (subvolume or snapshot)')
+          ('i', 'id'),                                      # id
+          ('p', 'parent\'s id'),                            # pid
+          ('n', 'name'),                                    # name
+          ('l', 'location'),                                # loc
+          ('t', 'datetime'),                                # time
+          ('m', 'mount type'),                              # mnt
+          ('r', 'permissions (read-write or read-only)'),   # read
+          ('s', 'type (subvolume or snapshot)')             # snap
         ]
 
 config_path = '$HOME/.config/subv/sources.cfg'
@@ -34,14 +35,22 @@ def get_args(self):
     args = self.parse_args()
     match(args.cmd):
         case 'list':
+            args.filter_paths = True
+            if args.path is None:
+                args.path = '/'
+                args.filter_paths = False
+            if args.explicit_filter is not None:
+                args.filter_paths = args.explicit_filter
             if not os.path.exists(args.path):
                 self.subs['list'].error(f'path [{args.path}] not found')
+            if os.stat(args.path).st_ino != 256:
+                self.subs['list'].error(f'path [{args.path}] is not btrfs')
             field_str = ''
             for f in fields: field_str += f[0]
             field_reg = f'^[{field_str}]+$'
             if not re.match(field_reg, args.fields):
                 self.subs['list'].error(f'unknown value [{args.fields}] for FIELDS')
-            if not re.match(field_reg, args.order):
+            if not re.match(field_reg, args.order, re.IGNORECASE):
                 self.subs['list'].error(f'unknown value [{args.order}] for ORDER')
             if args.all_fields:
                 args.fields = field_str
@@ -94,14 +103,16 @@ def get_parser():
     main.subs['list'] = listp
     listp_help = 'R|FIELDS is a series of letters that refer to the fields that will be displayed and in what order. Default is \'il\'. Fields can be:'
     for f in fields: listp_help += f'\n  {f[0]}  {f[1]}'
-    listp.add_argument('path', nargs='?', default='/', help='list all subvolumes under PATH. Default is /')
-    listp.add_argument('-o', '--order', default='i', help='ORDER is a series of letters indicating which fields to sort by. Default is \'i\'')
+    listp.add_argument('path', nargs='?', help='list all subvolumes under PATH. If PATH isn\'t supplied, it defaults to \'/\', but no path filtering is applied')
+    listp_path = listp.add_mutually_exclusive_group()
+    listp_path.add_argument('-p', '--filter-paths', dest='explicit_filter', action='store_const', const=True, help='only display subvolumes whose location is under PATH')
+    listp_path.add_argument('-P', '--no-filter-paths', dest='explicit_filter', action='store_const', const=False, help='displays all subvolumes without filtering by path')
     listp.add_argument('-a', '--all-fields', action='store_true', help='all fields will be displayed in their default order. Overrides FIELDS if set')
     listp.add_argument('-f', '--fields', default='il', help=listp_help)
-    listp.add_argument('-u', '--skip-path-filter', action='store_true', help='don\'t filter results by path')
+    listp.add_argument('-o', '--order', default='i', help='ORDER is a series of letters indicating which fields to sort by. Uppercase letters will reverse the sort order. Default is \'i\'')
     listp_mount = listp.add_mutually_exclusive_group()
-    listp_mount.add_argument('-m', '--mounted', action='store_true', help='filter to show subvolumes that are explicitly mounted')
-    listp_mount.add_argument('-M', '--unmounted', action='store_true', help='filter to show subvolumes that are NOT explicitly mounted')
+    listp_mount.add_argument('-m', '--mounted', action='store_true', help='filter to show subvolumes that are mounted')
+    listp_mount.add_argument('-M', '--unmounted', action='store_true', help='filter to show subvolumes that are NOT mounted')
     listp_read = listp.add_mutually_exclusive_group()
     listp_read.add_argument('-r', '--read-only', action='store_true', help='filter to show subvolumes that are read-only')
     listp_read.add_argument('-R', '--read-write', action='store_true', help='filter to show subvolumes that are NOT read-only')
@@ -145,7 +156,7 @@ def read_config():
 
 ###### A helper function to execute a command and return it's results in a string
 def execute(cmd):
-    return subprocess.run(cmd.split(), stdout=subprocess.PIPE).stdout.decode('utf-8')
+    return subprocess.run(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.decode('utf-8')
 
 
 ###### Returns a list of subvolumes as a tuple consisting of id, parent's id, and name
@@ -156,7 +167,7 @@ def get_subs(path):
         field = line.split()
         field[8] = re.sub(r'^<FS_TREE>/', '', field[8])
         subvolumes.append((field[1], field[6], field[8]))
-    return subvolumes
+    return subvolumes # (id, pid, loc)
 
 
 ###### Returns a list of subvolume ids for subvolumes that are snapshots
@@ -166,7 +177,7 @@ def get_snaps(path):
     for line in results.splitlines():
         field = line.split()
         snaps.append(field[1])
-    return snaps
+    return snaps # (id)
 
 
 ###### Returns a list of subvolume ids for subvolumes that are read-only
@@ -176,7 +187,7 @@ def get_ros(path):
     for line in results.splitlines():
         field = line.split()
         ros.append(field[1])
-    return ros
+    return ros # (id)
 
 
 ###### Returns a list of mounted subvolumes as a tuple consisting of id, name, and mount point
@@ -185,10 +196,26 @@ def get_mounts():
     mounts = []
     for line in results.splitlines():
         field = line.split()
-        match_id = re.search(r'subvolid=(\d+)', field[3])
-        match_name = re.search(r'\[/(.*?)\]', field[1])
-        mounts.append((match_id.group(1), match_name.group(1), field[0]))
-    return mounts
+        match_id = re.search(r'(?<=\bsubvolid=)\d+', field[3])
+        match_name = re.search(r'(?<=\bsubvol=/)[^,]*', field[3])
+        mounts.append((match_id.group(0), match_name.group(0), field[0]))
+    return mounts # (id, name, loc)
+
+
+###### Returns details from a subvolume
+def get_details(path):
+    results = execute(f'sudo btrfs subvolume show {path}')
+    match_name = re.search(r'Name:\s+(.+)', results)
+    match_time = re.search(r'Creation time:\s+(.+)', results)
+    match_err = re.search(r'ERROR: not a btrfs filesystem', results)
+    name = match_name.group(1) if match_name else ''
+    time = match_time.group(1) if match_time else ''
+    mount = False
+    if match_err:
+        name = '?'
+        time = '?'
+        mount = True
+    return (name, time, mount)
 
 
 ###### Creates a 2d array of subvolume info by combining the output of multiple system calls
@@ -207,22 +234,24 @@ def get_subvolumes(path):
         if s[0] in ros:
             r = 'r'
             ros.remove(s[0])
-        mnt = 'no'
+        mnt = '-'
         loc = ''
         for m in mounts:
             if m[0] == s[0]:
-                mnt = 'yes'
+                mnt = 'btrfs'
                 loc = m[2]
                 break
-        if mnt == 'no':
+        if mnt == '-':
             parent_dir = s[2].split('/')[0] # substring of name before first /
             for m in mounts:
                 if parent_dir == m[1]:
                     loc = s[2].replace(parent_dir, m[2])
                     if m[2] == '/': loc = loc[1:]
                     break
-        subvolumes.append((int(s[0]), int(s[1]), s[2], loc, mnt, r, typ))
-    return subvolumes
+        details = get_details(loc)
+        if details[2]: mnt = execute(f'findmnt -nl {loc}').split()[2]
+        subvolumes.append((int(s[0]), int(s[1]), details[0], loc, details[1], mnt, r, typ))
+    return subvolumes # (id, pid, name, loc, time, mnt, read, snap)
 
 
 ###### Prints a 2d array to stdout
@@ -247,24 +276,27 @@ def print_2d(arr, spaces=1):
 
 ###### Display list of subvolumes
 def exec_list(args):
-    subs = get_subvolumes(args.path)
-    if not args.skip_path_filter: subs = [s for s in subs if s[3].startswith(args.path)]
-    if args.read_only: subs = [s for s in subs if s[5] == 'r']
-    if args.read_write: subs = [s for s in subs if s[5] != 'r']
-    if args.mounted: subs = [s for s in subs if s[4] == 'yes']
-    if args.unmounted: subs = [s for s in subs if s[4] == 'no']
-    if args.snapshot: subs = [s for s in subs if s[6] == 'snapshot']
-    if args.subvolume: subs = [s for s in subs if s[6] == 'subvolume']
+    subs = get_subvolumes(args.path) # (id, pid, name, loc, time, mnt, read, snap)
+    if args.filter_paths: subs = [s for s in subs if s[3].startswith(args.path)]
+    if args.read_only: subs = [s for s in subs if s[6] == 'r']
+    if args.read_write: subs = [s for s in subs if s[6] != 'r']
+    if args.mounted: subs = [s for s in subs if s[5] != '-']
+    if args.unmounted: subs = [s for s in subs if s[5] == '-']
+    if args.snapshot: subs = [s for s in subs if s[7] == 'snapshot']
+    if args.subvolume: subs = [s for s in subs if s[7] == 'subvolume']
+    field_dictionary = {}
     for i,f in enumerate(fields):
-        args.fields = args.fields.replace(f[0], str(i))
-        args.order = args.order.replace(f[0], str(i))
-    for s in reversed(args.order):
-        subs.sort(key = lambda x: x[int(s)])
+        field_dictionary[f[0]] = i
+    for o in reversed(args.order):
+        if o.islower():
+            subs.sort(key = lambda x: x[field_dictionary[o]])
+        else:
+            subs.sort(key = lambda x: x[field_dictionary[o.lower()]], reverse = True)
     out = []
     for s in subs:
         out.append([])
         for f in args.fields:
-            out[-1].append(s[int(f)])
+            out[-1].append(s[field_dictionary[f]])
     print_2d(out, 3)
     exit(0)
 
