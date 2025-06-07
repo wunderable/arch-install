6a639497b177e85ccad44da9c6e14495a5d50665
wunderable <33606930+wunderable@users.noreply.github.com>
2025-06-07 14:33:48 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -308,7 +308,7 @@ sed -Ei "s/^# (%wheel ALL=\(ALL:ALL\) ALL)/\1/" /etc/sudoers
 sed -i "s/#Color/Color/" /etc/pacman.conf
 
 # Install packages with pacman
-pacman --noconfirm -S acpid openssh python3 zsh # upower pipewire wireplumber alsa-utils
+pacman --noconfirm -S acpid openssh python3 zsh zsh-autosuggestions # upower pipewire wireplumber alsa-utils
 if [[ "<$GUI>" == "y" ]]; then
 	pacman --noconfirm -S greetd greetd-tuigreet gtk4 hyprland kitty neofetch spotify-launcher ttf-joypixels ttf-roboto-mono-nerd vivaldi vivaldi-ffmpeg-codecs
 fi
