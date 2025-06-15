e6a2bb9a4f9c5f3e5be6cd9a7e75bbcc96491fdd
wunderable <33606930+wunderable@users.noreply.github.com>
2025-06-15 18:50:12 -0500

    Update prompt.sh

--- files/shell/prompt.sh
+++ files/shell/prompt.sh
@@ -1,13 +1,13 @@
-source /usr/share/git/git-prompt.sh
+#source /usr/share/git/git-prompt.sh
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWSTASHSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
 COLOR=$(( EUID == 0 ? 31 : 32))
 
-USER="\[\e[${COLOR}m\]\u"
-HOST="\[\e[90m\]@\h:"
-DIR="\[\e[34m\]\w"
-GIT='\[\e[33m\]$(__git_ps1)'
-CHAR="\[\e[${COLOR}m\]\$\[\e[0m\] "
+PS1_USER="\[\e[${COLOR}m\]\u"
+PS1_HOST="\[\e[90m\]@\h:"
+PS1_DIR="\[\e[34m\]\w"
+#PS1_GIT='\[\e[33m\]$(__git_ps1)'
+PS1_PROMPT="\[\e[${COLOR}m\]\$\[\e[0m\] "
 
-export PS1="${USER}${HOST}${DIR}${GIT}${CHAR}"
+export PS1="${PS1_USER}${PS1_HOST}${PS1_DIR}${PS1_PROMPT}"
